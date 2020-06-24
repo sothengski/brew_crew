9938b2352d002facfc89d0537b46ab48fd7f4f61
HBOffice <HBOffice@SothengChheangs-MacBook-Pro.local>
2020-06-24 16:42:01 +0700

    init commit

---
+++ lib/screens/home/brew_list.dart
@@ -0,0 +1,34 @@
+import 'package:brew_crew/models/brew.dart';
+import 'package:brew_crew/screens/home/brew_tile.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class BrewList extends StatefulWidget {
+  @override
+  _BrewListState createState() => _BrewListState();
+}
+
+class _BrewListState extends State<BrewList> {
+  @override
+  Widget build(BuildContext context) {
+    // final brews = Provider.of<QuerySnapshot>(context);
+    final brews = Provider.of<List<Brew>>(context) ?? [];
+    // brews.forEach((brew) {
+    //   print(brew.name);
+    //   print(brew.sugars);
+    //   print(brew.strength);
+    // });
+
+    // print(brews.documents);
+    // for (var doc in brews.documents) {
+    //   print(doc.data);
+    // }
+    return ListView.builder(
+      itemCount: brews.length,
+      itemBuilder: (context, index) {
+        return BrewTile(brew: brews[index]);
+      },
+    );
+  }
+}
